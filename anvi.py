040b83357a7caf52234325b7add1bf8876226b2b
chirag r rai <chiragrrai44@gmail.com>
2024-10-07 14:53:13 +0530

    Add files via upload

---
+++ anvi.py
@@ -0,0 +1,16 @@
+import turtle
+
+t = turtle.Turtle()
+turtle.bgcolor("Blue")
+t.pensize(5)
+t.color("Red")
+t.forward(100) 
+t.left(90)     
+t.circle(50)
+t.penup()           
+t.goto(-50, -50)
+t.pendown()
+t.forward(100)
+
+turtle.done()       
+
